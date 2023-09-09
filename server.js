14f0b510fcd6319f06ecf20766bb8307867e9510
D3N2-A <anmolsharma999937@gmail.com>
2023-09-09 12:27:13 +0530

    Server Added

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const https = require("https");
+const app = express();
+let fs = require("fs");
+let options = {
+  pfx: fs.readFileSync("./certificate.pfx"),
+  passphrase: "1012",
+};
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("hello");
+});
+app.get("/messaging-webhook", (req, res) => {
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === "hello1234") {
+      console.log("WEBHOOK_VERIFIED");
+      res.status(200).send(challenge);
+    } else {
+      res.sendStatus(403);
+    }
+  }
+});
+
+let secure = https.createServer(options, app);
+
+secure.listen(port, function () {
+  console.log("localhost started on", port);
+});
