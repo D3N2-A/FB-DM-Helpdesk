bb1e2a25e4a0890daf80aa3e41e2d9ff56c37f95
D3N2-A <anmolsharma999937@gmail.com>
2023-09-30 01:09:35 +0530

    Integration Complete

--- src/app/configure/page.js
+++ src/app/configure/page.js
@@ -15,36 +15,46 @@ function Configure() {
   const [user, setUser] = useRecoilState(userState);
   const handleConnect = async () => {
     initFacebookSdk().then(() => {
-      fbLogin().then((response) => {
-        console.log(response);
-        if (response.status === "connected") {
-          console.log("Person is connected");
-          localStorage.setItem(
-            "user",
-            JSON.stringify({
-              status: "connected",
-              accessToken: response?.authResponse?.accessToken,
-              userID: response?.authResponse?.userID,
-            })
-          );
+      fbLogin()
+        .then((response) => {
+          console.log(response);
+          if (response.status === "connected") {
+            console.log("Person is connected");
+            localStorage.setItem(
+              "user",
+              JSON.stringify({
+                status: "connected",
+                accessToken: response?.authResponse?.accessToken,
+                userID: response?.authResponse?.userID,
+              })
+            );
+            localStorage.setItem("userId", response?.authResponse?.userID);
+            localStorage.setItem(
+              "userToken",
+              response?.authResponse?.accessToken
+            );
 
-          setUser({
-            authenticated: true,
-            userID: response?.authResponse?.userID,
-            accessToken: response?.authResponse?.accessToken,
-            isLoading: false,
-          });
-          setIsConnected(true);
-          toast.success("Page integrated successfully", toastSuccess);
-          getConversations();
-        } else {
-          console.log("Some Error Occured");
-        }
-      });
+            setUser({
+              authenticated: true,
+              userID: response?.authResponse?.userID,
+              accessToken: response?.authResponse?.accessToken,
+              isLoading: false,
+            });
+            setIsConnected(true);
+            toast.success("Page integrated successfully", toastSuccess);
+          } else {
+            console.log("Some Error Occured");
+          }
+        })
+        .finally(() => {
+          getConversations(user.accessToken, user.userID);
+        });
     });
   };
 
-  const handleIntegration = () => {
+  const handleIntegration = (page) => {
+    localStorage.setItem("pageToken", page?.access_token);
+    localStorage.setItem("pageId", page?.id);
     router.push("/conversations");
   };
   const deleteIntegration = () => {
@@ -54,23 +64,34 @@ function Configure() {
 
   //---------------------Fetching Pages info---------------------------//
   const [pages, setPages] = useState([]);
-  const getConversations = async () => {
-    const res = await fetch(
-      `https://graph.facebook.com/v17.0/${user.userID}/accounts?access_token=${user.accessToken}`
-    )
-      .then((res) => {
-        res
-          .json()
-          .then((val) => {
-            setPages(val?.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      })
-      .catch((err) => {
-        toast.warn("Some error occured", toastSuccess);
-      });
+  const getConversations = async (
+    token = localStorage.getItem("userToken"),
+    id = localStorage.getItem("userId")
+  ) => {
+    try {
+      fetch(
+        `https://graph.facebook.com/v17.0/${id}/accounts?access_token=${token}`
+      )
+        .then((res) => {
+          if (res.status === 400) {
+            deleteIntegration();
+          } else {
+            res
+              .json()
+              .then((val) => {
+                setPages(val?.data);
+              })
+              .catch((err) => {
+                console.log(err);
+              });
+          }
+        })
+        .catch((err) => {
+          toast.warn("Some error occured", toastSuccess);
+        });
+    } catch (error) {
+      toast.warn("Tring to Fetch Pages", toastSuccess);
+    }
   };
 
   return (
@@ -90,10 +111,12 @@ function Configure() {
                 pages.map((page, index) => {
                   return (
                     <>
-                      {" "}
                       <div
+                        key={index}
                         className={styles.btnPrimary}
-                        onClick={handleIntegration}
+                        onClick={() => {
+                          handleIntegration(page);
+                        }}
                       >
                         {page?.name}{" "}
                         <div className={styles.category}>{page?.category}</div>
